8ac7f17770f946831e8ee7484c8e001b1884fac2
BrianCodingRun <dev.contact@briancoupama.re>
2025-04-11 15:25:31 +0400

    Récupération de plus d'utilisateur et afficher tous les users

--- script.js
+++ script.js
@@ -12,17 +12,11 @@ const loader = document.querySelector(".loader");
 // Fonction pour récupérer les infos du profil
 const fetchUserData = async () => {
   try {
-    card.classList.add("hidden");
-    loader.classList.add("show");
-
-    const response = await fetch("https://randomuser.me/api/");
+    const response = await fetch("https://randomuser.me/api/?results=20");
     const data = await response.json();
+    console.log(data);
     if (response.ok) {
       updateUI(data);
-      setTimeout(() => {
-        loader.classList.remove("show");
-        card.classList.remove("hidden");
-      }, 2000);
     }
   } catch (error) {
     console.log(error);
@@ -34,17 +28,39 @@ fetchUserData();
 // Afficher les données reçu dans le dom
 const updateUI = (data) => {
   if (data.results.length) {
-    const imgSrc = data.results[0].picture.large;
-    const title = data.results[0].name.first + " " + data.results[0].name.last;
-    const age = data.results[0].dob.age;
-    const location = data.results[0].location.city;
-    const email = data.results[0].email;
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    document.body.appendChild(grid);
+    for (let i = 0; i < data.results.length; i++) {
+      const imgSrc = data.results[i].picture.large;
+      const title =
+        data.results[i].name.first + " " + data.results[i].name.last;
+      const age = data.results[i].dob.age;
+      const location = data.results[i].location.city;
+      const email = data.results[i].email;
 
-    cardHeaderImg.src = imgSrc;
-    cardHeaderImg.alt = title;
-    cardBodyTitle.innerHTML = title;
-    cardBodyAge.innerHTML = age;
-    cardBodyLocation.innerHTML = location;
-    cardBodyEmail.innerHTML = email;
+      const card = `
+        <div class="card">
+        <div class="card-header">
+          <img
+            src="${imgSrc}"
+            class="card-header__img"
+            alt="${title}"
+          />
+        </div>
+        <div class="card-body">
+          <h5 class="card-body__title">${title}</h5>
+          <div class="card-body__infos">
+            <p class="card-body__infos_age">Age: <span>${age}</span></p>
+            <p class="card-body__infos_location">Location: <span>${location}</span></p>
+            <p class="card-body__infos_email">
+              Email: <span>${email}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      `;
+      grid.insertAdjacentHTML("beforeend", card);
+    }
   }
 };
